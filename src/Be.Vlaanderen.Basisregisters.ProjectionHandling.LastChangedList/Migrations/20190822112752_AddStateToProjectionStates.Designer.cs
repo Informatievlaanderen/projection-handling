61670cc2a0f2321f02737125a01a322c6575ea40
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-08-22 13:30:06 +0200

    feat: add migration to save states of projectionstates

--- src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/Migrations/20190822112752_AddStateToProjectionStates.Designer.cs
+++ src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/Migrations/20190822112752_AddStateToProjectionStates.Designer.cs
@@ -4,14 +4,16 @@ using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList.Migrations
 {
     [DbContext(typeof(LastChangedListContext))]
-    partial class LastChangedListContextModelSnapshot : ModelSnapshot
+    [Migration("20190822112752_AddStateToProjectionStates")]
+    partial class AddStateToProjectionStates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
