0e7bf6d45e381b7465f23f000cd404914daf56af
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-03-31 15:51:32 +0200

    feat: add last error message to last changed list

---
+++ src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/Migrations/20200331134944_AddLastErrorMessage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList.Migrations
+{
+    public partial class AddLastErrorMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastErrorMessage",
+                schema: "Redis",
+                table: "LastChangedList",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastErrorMessage",
+                schema: "Redis",
+                table: "LastChangedList");
+        }
+    }
+}
