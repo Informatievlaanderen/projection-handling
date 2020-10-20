bfa35b059938b41f87dd0a907c226d5d48b9976a
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-10-20 09:21:50 +0200

    feat: add errormessage to projection state lastchangedlist

--- src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/Migrations/20201020071924_AddErrorMessage.Designer.cs
+++ src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/Migrations/20201020071924_AddErrorMessage.Designer.cs
@@ -4,14 +4,16 @@ using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList.Migrations
 {
     [DbContext(typeof(LastChangedListContext))]
-    partial class LastChangedListContextModelSnapshot : ModelSnapshot
+    [Migration("20201020071924_AddErrorMessage")]
+    partial class AddErrorMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
