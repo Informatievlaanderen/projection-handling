926e84cc8d658c3830ee2af505c01fbcb692e04e
Arne Dumarey <dumarey.arne@gmail.com>
2023-01-05 19:55:47 +0100

    feat: add direct envelope microsoft ioc registration

---
+++ src/Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Microsoft/EnvelopeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Microsoft
+{
+    using EventHandling;
+    using global::Microsoft.Extensions.DependencyInjection;
+
+    public static class EnvelopeExtensions
+    {
+        public static IServiceCollection ConfigureEnvelope(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<EnvelopeFactory>(_ => new EnvelopeFactory(_.GetRequiredService<EventMapping>(), _.GetRequiredService<EventDeserializer>()));
+            
+            return serviceCollection;
+        }
+    }
+}
