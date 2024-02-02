c8e04238f6f741b86eae81755e503ee3126e04b0
Arne Dumarey <dumarey.arne@gmail.com>
2024-02-02 08:32:21 +0100

    feat: add cachevalidator wether to update position

---
+++ src/Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList/ICacheValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface ICacheValidator
+    {
+        Task<bool> CanCache(long position, CancellationToken ct);
+    }
+}
